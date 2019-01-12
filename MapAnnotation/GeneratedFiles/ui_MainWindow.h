9ec8b206ccd0f9ba9e1a2003f900eeb64a94c414
gnishida <gnishida@purdue.edu>
2019-01-12 10:06:26 -0800

    update

--- MapAnnotation/GeneratedFiles/ui_MainWindow.h
+++ MapAnnotation/GeneratedFiles/ui_MainWindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'MainWindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.6.1
+** Created by: Qt User Interface Compiler version 5.6.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -84,15 +84,15 @@ public:
 
     void retranslateUi(QMainWindow *MainWindowClass)
     {
-        MainWindowClass->setWindowTitle(QApplication::translate("MainWindowClass", "Map Annotation", 0));
-        actionExit->setText(QApplication::translate("MainWindowClass", "Exit", 0));
-        actionOpenImage->setText(QApplication::translate("MainWindowClass", "Open Image", 0));
-        actionOpenXML->setText(QApplication::translate("MainWindowClass", "Open XML", 0));
-        actionOpenXML->setShortcut(QApplication::translate("MainWindowClass", "Ctrl+O", 0));
-        actionSaveXML->setText(QApplication::translate("MainWindowClass", "Save XML", 0));
-        actionSaveXML->setShortcut(QApplication::translate("MainWindowClass", "Ctrl+S", 0));
-        actionOpenOSM->setText(QApplication::translate("MainWindowClass", "Open OSM", 0));
-        menuFile->setTitle(QApplication::translate("MainWindowClass", "File", 0));
+        MainWindowClass->setWindowTitle(QApplication::translate("MainWindowClass", "Map Annotation", Q_NULLPTR));
+        actionExit->setText(QApplication::translate("MainWindowClass", "Exit", Q_NULLPTR));
+        actionOpenImage->setText(QApplication::translate("MainWindowClass", "Open Image", Q_NULLPTR));
+        actionOpenXML->setText(QApplication::translate("MainWindowClass", "Open XML", Q_NULLPTR));
+        actionOpenXML->setShortcut(QApplication::translate("MainWindowClass", "Ctrl+O", Q_NULLPTR));
+        actionSaveXML->setText(QApplication::translate("MainWindowClass", "Save XML", Q_NULLPTR));
+        actionSaveXML->setShortcut(QApplication::translate("MainWindowClass", "Ctrl+S", Q_NULLPTR));
+        actionOpenOSM->setText(QApplication::translate("MainWindowClass", "Open OSM", Q_NULLPTR));
+        menuFile->setTitle(QApplication::translate("MainWindowClass", "File", Q_NULLPTR));
     } // retranslateUi
 
 };
